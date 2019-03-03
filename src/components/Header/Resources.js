a4ac0bf957abbb52f39915a8c965b0c52706f6ec
Paulo Soares <kabal.dev.pt@gmail.com>
2019-03-03 17:02:28 +0000

    Add Resources comp

--- src/components/Header/Resources.js
+++ src/components/Header/Resources.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { Popover } from '../';
 
-const Resources = () => {
-  return (
-    <div>
-      Resources
-    </div>
-  );
+import { Icon } from '../../theme/components';
+
+import {
+    ResourcesCategoriesList,
+    ResourcesCategoriesItem,
+    ResourcesCategoriesLabel,
+    ResourcesSublist,
+    ResourcesSublistItem,
+    ResourcesLink,
+} from './resources.style';
+
+const Resources = ({ categories, label }) => {
+    return (
+        <Popover
+            alternative
+            content={(
+                <ResourcesCategoriesList>
+                    {categories.map(({ icon, label, items }, i) => (
+                        <ResourcesCategoriesItem key={i}>
+                            <ResourcesCategoriesLabel>
+                                <Icon icon={icon} size={2} />
+                                <span>
+                                    {label}
+                                </span>
+                            </ResourcesCategoriesLabel>
+                            <ResourcesSublist>
+                                {items.map(({ label, icon, url }, ii) => (
+                                    <ResourcesSublistItem key={ii}>
+                                        <ResourcesLink
+                                            href={url}
+                                            target="_blank"
+                                            rel="noopner"
+                                        >
+                                            <Icon icon={icon} size={1} />
+                                            <span>
+                                                {label}
+                                            </span>
+                                        </ResourcesLink>
+                                    </ResourcesSublistItem>
+                                ))}
+                            </ResourcesSublist>
+                        </ResourcesCategoriesItem>
+                    ))}
+                </ResourcesCategoriesList>
+            )}
+        >
+            {label}
+        </Popover>
+    );
 };
 
 export default Resources;
