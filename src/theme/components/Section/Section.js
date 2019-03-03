5a78ac48013128a98adb1bed0de031b9d8569011
Paulo Soares <kabal.dev.pt@gmail.com>
2019-03-03 10:43:24 +0000

    Update Section component to be extended with react-scroll Element

--- src/theme/components/Section/Section.js
+++ src/theme/components/Section/Section.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 import { generateProps } from 'styled-gen';
+import { Element } from 'react-scroll';
 
-const Section = styled.div`
+const Section = styled(Element)`
     ${generateProps};
 `;
+
+Section.defaultProps = {
+    as: 'section',
+};
 
 export default Section;
