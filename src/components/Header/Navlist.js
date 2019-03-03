ab09cfe1ac7c27eae155347ac5b653fce7e6727a
Paulo Soares <kabal.dev.pt@gmail.com>
2019-03-03 10:47:42 +0000

    Add menu list no Navlist

--- src/components/Header/Navlist.js
+++ src/components/Header/Navlist.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link as ScrollLink } from 'react-scroll';
+import styled from 'styled-components';
+import { fonts, colors } from '../../theme';
 
-const Navlist = props => {
+const NavlistLinkContent = styled(ScrollLink)`
+    font-weight: ${fonts.weights.medium};
+    font-size: ${fonts.size('sm')};
+    color: ${colors.black};
+
+    &.active {
+        color: ${colors.primary};
+    }
+
+    & + & {
+        margin-left: 30px;
+    }
+`;
+
+const NavlistLink = ({name, label}) => (
+    <NavlistLinkContent
+        to={name}
+        spy
+        smooth
+        duration={500}
+        offset={-100}
+        activeClass="active"
+    >
+        {label}
+    </NavlistLinkContent>
+);
+
+const Navlist = ({ items }) => {
     /* eslint-disable-next-line */
-    console.log(props);
-    return (
-        <div>
-            Navlist
-        </div>
-    );
+    console.log(items);
+    return items.map((item, i) => <NavlistLink key={i} {...item} />);
 };
 
 Navlist.propTypes = {
